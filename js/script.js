8889e790dfdefb3cbfada11ae21c12b410da0f7f
EMC-prog <65492923+EMC-prog@users.noreply.github.com>
2022-04-27 08:46:57 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
+  navigator.serviceWorker.register('/recipes-2.0/sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
